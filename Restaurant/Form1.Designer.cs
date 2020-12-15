1ef5c9944d5dfb3775901fdc8fb0c01724791974
Ousman-Bagoudou-HAMADOU <ousman.hamadou@2023.ucac-icam.com>
2020-12-15 15:07:56 +0100

    Mise en forme du Form et envoi des images

--- Restaurant/Form1.Designer.cs
+++ Restaurant/Form1.Designer.cs
@@ -29,13 +29,383 @@ namespace Restaurant
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtTmp = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.picCR = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.picMH = new System.Windows.Forms.PictureBox();
+            this.picSR = new System.Windows.Forms.PictureBox();
+            this.picTB = new System.Windows.Forms.PictureBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.picCS = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCR)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Goldenrod;
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(727, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 436);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Comptoir";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 365);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Serviette + Nappe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Couverts et Assiettes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Commande Client";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Eau + Pain";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MENU";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(893, 325);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(393, 146);
+            this.dataGridView1.TabIndex = 38;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(1106, 61);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 42);
+            this.button10.TabIndex = 37;
+            this.button10.Text = "Nettoyage Table";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1211, 61);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 42);
+            this.button8.TabIndex = 36;
+            this.button8.Text = "Nouvelle Nappe";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1002, 61);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 42);
+            this.button6.TabIndex = 35;
+            this.button6.Text = "Départ du Client";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(893, 61);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 42);
+            this.button5.TabIndex = 34;
+            this.button5.Text = "Début avec le serveur";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // txtTmp
+            // 
+            this.txtTmp.Location = new System.Drawing.Point(870, 23);
+            this.txtTmp.Name = "txtTmp";
+            this.txtTmp.Size = new System.Drawing.Size(75, 20);
+            this.txtTmp.TabIndex = 33;
+            this.txtTmp.Text = "0";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(960, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 20);
+            this.button4.TabIndex = 32;
+            this.button4.Text = "Définir";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // picCR
+            // 
+            this.picCR.Location = new System.Drawing.Point(230, 23);
+            this.picCR.Name = "picCR";
+            this.picCR.Size = new System.Drawing.Size(39, 31);
+            this.picCR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCR.TabIndex = 29;
+            this.picCR.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(893, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(399, 159);
+            this.groupBox2.TabIndex = 31;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Légende";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(179, 113);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(214, 25);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "CS: Commis de Salle";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(14, 68);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(105, 25);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "TB: Table";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(179, 68);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(186, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "CR: Chef de Rang";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(14, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 25);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "SR: Serveur";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(179, 16);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(186, 25);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "MH: Maître d\'hôtel";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "CT: Client";
+            // 
+            // picMH
+            // 
+            this.picMH.Location = new System.Drawing.Point(95, 208);
+            this.picMH.Name = "picMH";
+            this.picMH.Size = new System.Drawing.Size(39, 31);
+            this.picMH.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picMH.TabIndex = 27;
+            this.picMH.TabStop = false;
+            // 
+            // picSR
+            // 
+            this.picSR.Location = new System.Drawing.Point(230, 117);
+            this.picSR.Name = "picSR";
+            this.picSR.Size = new System.Drawing.Size(39, 31);
+            this.picSR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picSR.TabIndex = 26;
+            this.picSR.TabStop = false;
+            // 
+            // picTB
+            // 
+            this.picTB.Location = new System.Drawing.Point(95, 108);
+            this.picTB.Name = "picTB";
+            this.picTB.Size = new System.Drawing.Size(39, 31);
+            this.picTB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picTB.TabIndex = 25;
+            this.picTB.TabStop = false;
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(95, 23);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(39, 31);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pic.TabIndex = 24;
+            this.pic.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1149, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Fermer";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1052, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // picCS
+            // 
+            this.picCS.Location = new System.Drawing.Point(95, 325);
+            this.picCS.Name = "picCS";
+            this.picCS.Size = new System.Drawing.Size(39, 31);
+            this.picCS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCS.TabIndex = 28;
+            this.picCS.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1387, 520);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txtTmp);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.picCR);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.picMH);
+            this.Controls.Add(this.picSR);
+            this.Controls.Add(this.picTB);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.picCS);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCR)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        public System.Windows.Forms.TextBox txtTmp;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox picCR;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox picMH;
+        private System.Windows.Forms.PictureBox picSR;
+        private System.Windows.Forms.PictureBox picTB;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox picCS;
     }
 }
 
