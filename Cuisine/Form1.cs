5103cfab72c06bafc86a063e2904cc855e47e019
Armel Ndoumab <armel.ndoumba@2023.ucac-icam.com>
2020-12-15 16:17:23 +0100

    Initialisation de variables

--- Cuisine/Form1.cs
+++ Cuisine/Form1.cs
@@ -10,11 +10,28 @@ using System.Windows.Forms;
 
 namespace Cuisine
 {
-    public partial class Form1 : Form
+    public partial class Cuisine : Form
     {
-        public Form1()
+        SimpleTcpServer server;
+        int tmp = 0;
+        int okc = 0;
+        int tmpajt = 5;
+        int np = 0;
+        public Cuisine()
         {
             InitializeComponent();
+            Cuisiniere.Location = new Point(1328, 150);
+            ChefCuisine.Location = new Point(430, 55);
+            ChefPartie.Location = new Point(500, 300);
+            CommisCuisine.Location = new Point(1000, 100);
+            LaveLinge.Location = new Point(1000, 450);
+            LaveVaisselle.Location = new Point(1150, 450);
+            Plongeur.Location = new Point(1150, 300);
+            //Plongeur.Location = new Point(1229, 300);
+            StockAliment.Location = new Point(1328, 10);
+            StockAssiette.Location = new Point(1328, 300);
+            StockLingeSale.Location = new Point(850, 450);
+            StockAssietteSale.Location = new Point(1328, 450);
+            
         }
-    }
 }
